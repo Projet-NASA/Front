bdfe4463c2fbcfc64b689c60b10fde462a2540c0
Turbotard <turbotard9@gmail.com>
2024-03-05 14:39:51 +0100

    Refactor button to use NuxtLink component (faster, easier, better, stronger)

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,8 @@
 <template>
-  <div>
-    <button @click="redirectToForm">Aller au formulaire</button>
+  <div class="flex justify-center items-center h-screen">
+    <NuxtLink to="/form" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      Aller au formulaire
+    </NuxtLink>
   </div>
+
 </template>
-
-<script setup>
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-function redirectToForm() {
-  router.push('/form');
-}
-</script>
