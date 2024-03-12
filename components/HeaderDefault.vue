2fa15db840a7f0da029bcfac5d594354ea122965
KyoShred <lucas.salaun2@gmail.com>
2024-03-12 19:23:00 +0100

    Add default layout and header component

---
+++ components/HeaderDefault.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <head>
+      <title>AutoLinkedIn</title>
+    </head>
+    <header class="bg-purple-950 shadow-lg">
+        <div class="container flex flex-row items-center justify-between">
+            <div class="c-header__logo flex items-center">
+              <div>
+
+              </div>
+            </div>
+            <ul class="flex flex-row">
+              <li>
+                
+              </li>
+            </ul>
+        </div>
+
+    </header>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="flex justify-center items-center h-screen">
-    <NuxtLink
-      to="/form"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-    >
-      Aller au formulaire
-    </NuxtLink>
+    
   </div>
 </template>
