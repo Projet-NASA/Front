74dd61a0979184716a4f7713c42292249675fc28
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-27 10:18:54 +0100

    feat: Add dependencies for jsonwebtoken and @types/jsonwebtoken

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,18 @@
-import { useRouter } from 'vue-router'
+import jwt from "jsonwebtoken";
+import { useRouter } from "vue-router";
+const SECRET_KEY = "your-secret-key";
 
 export function checkTokenAndRedirect() {
-  const router = useRouter()
-  if (localStorage.getItem('token')) {
-    router.push('/')
+  const router = useRouter();
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    try {
+      jwt.verify(token, SECRET_KEY);
+      router.push("/");
+    } catch (err) {
+      console.error("Token has been tampered with", err);
+    }
   }
 }
+ 
