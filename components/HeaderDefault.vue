6c12b2251bb9064b3c473c1edadf4db45e59bdbe
Stanewolfer <seiya0500@gmail.com>
2024-03-28 14:10:12 +0100

    navbar responsive complète

--- components/HeaderDefault.vue
+++ components/HeaderDefault.vue
@@ -2,17 +2,23 @@
   <nav
     :class="['bg-secondary-400', 'shadow-inner', 'text-text-default', 'fixed', 'z-20', isFloating, 'left-0', 'right-0', 'mx-auto', 'transition-all duration-400 ease-in-out']">
     <div class="max-w-screen-xl flex flex-wrap justify-between items-center px-4 py-3 mx-auto space-y-3 md:space-y-0">
-      <nuxt-link to="/" class="text-text-default flex items-center space-x-3">
-        <img class="w-auto h-10 rounded-full" src="../public/logo-rounded.png" alt="Logo" />
-        <div class="text-text-default ml-2 font-bold text-xl">
-          AutoLinkedIn
+      <div class="flex items-center justify-between space-x-5 sm:space-x-10 md:space-x-20 lg:space-x-40">
+        <nuxt-link to="/" class="text-text-default flex items-center space-x-3">
+          <img class="w-5 h-5 lg:w-8 lg:h-8 xl:w-10 xl:h-10 rounded-full" src="../public/logo-rounded.png" alt="Logo" />
+          <div class="text-text-default lg:ml-2 font-bold text-sm md:text-base lg:text-xl xl:text-2xl">
+            AutoLinkedIn
+          </div>
+        </nuxt-link>
+        <div class="flex items-center">
+          <input type="text" placeholder="Search"
+            class="w-32 md:w-48 lg:w-64 xl:w-96 text-black px-4 py-2 border bg-white border-secondary-default rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary-300" />
         </div>
-      </nuxt-link>
-      <div class="flex items-center">
-        <input type="text" placeholder="Search"
-          class="text-black px-4 py-2 border bg-white border-secondary-default rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary-300" />
+        <button class="flex items-center sm:hidden" @click="developMenu">
+          <FontAwesomeIcon :icon="faChevronDown" class="block" id="develop"/>
+          <FontAwesomeIcon :icon="faChevronUp" class="hidden" id="hide" />
+        </button>
       </div>
-      <ul class="flex flex-row items-center space-x-10 text-2xl font-bold">
+      <ul class="hidden sm:flex flex-row mx-auto items-center space-x-5 lg:space-x-10 text-2xl font-bold" id="navbar">
         <li>
           <nuxt-link to="login" class="text-text-default">
             <span v-if="$route.path === '/login'">
@@ -60,7 +66,7 @@
 
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faSun, faMoon, faArrowRightToBracket, faPenToSquare, faEnvelope, faBell, faIdCard } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faArrowRightToBracket, faPenToSquare, faEnvelope, faBell, faIdCard, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { useCookie } from "#app";
 import { onMounted, ref } from 'vue';
 
@@ -94,4 +100,25 @@ const handleScroll = () => {
   }
 };
 
+
+const developMenu = () => {
+  const navbar = document.getElementById('navbar');
+  const develop = document.getElementById('develop');
+  const hide = document.getElementById('hide');
+  if (navbar.classList.contains('hidden')) {
+    navbar.classList.remove('hidden');
+    navbar.classList.add('flex');
+    develop.classList.remove('block');
+    develop.classList.add('hidden');
+    hide.classList.remove('hidden');
+    hide.classList.add('block');
+  } else {
+    navbar.classList.remove('flex');
+    navbar.classList.add('hidden');
+    develop.classList.remove('hidden');
+    develop.classList.add('block');
+    hide.classList.remove('block');
+    hide.classList.add('hidden');
+  }
+};
 </script>
