debfd9f7e41fe0efed7748d3774bcfd0d25b6254
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-03-29 10:02:26 +0100

    feat: Remove unused import and simplify token verification logic

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,18 +1,10 @@
-import jwt from 'jsonwebtoken'
 import { useRouter } from 'vue-router'
-const SECRET_KEY = 'U8lf@gQPKX4#xWp#eSH$'
 
 export function checkTokenAndRedirect() {
   const router = useRouter()
   const token = localStorage.getItem('token')
 
   if (token) {
-    try {
-      jwt.verify(token, SECRET_KEY)
-    } catch (err) {
-      console.error('le jeton a été falsifié', err)
-      localStorage.removeItem('token')
-      router.push('/inscription')
-    }
+    router.push('/')
   }
 }
