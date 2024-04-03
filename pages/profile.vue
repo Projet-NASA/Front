a7485c265b03bbfd960d340a642f6a58301c52c5
Turbotard <turbotard9@gmail.com>
2024-04-03 14:19:34 +0200

    Add logout button to profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -4,6 +4,7 @@
     <div v-if="user">
       <p>Email: {{ user.email }}</p>
       <p>Téléphone: {{ user.phone }}</p>
+      <button @click="logout">Déconnexion</button>
     </div>
     <div v-else>
       <p>Chargement des informations de l'utilisateur...</p>
@@ -12,40 +13,45 @@
 </template>
 
 <script>
-import apiURL from '../utils/apiURLs'
-
 export default {
   data() {
     return {
       user: null
-    }
+    };
   },
   async mounted() {
     await this.fetchUserData();
   },
   methods: {
     async fetchUserData() {
-      const userId = localStorage.getItem('userId');
-      if (!userId) {
-        console.error('Aucun ID utilisateur trouvé');
-        return;
-      }
       try {
-        const response = await fetch(`http://localhost:3003/user/OneUser/${userId}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          }
+        const response = await fetch('http://localhost:3003/user/session', {
+          credentials: 'include' 
         });
 
         if (!response.ok) {
-          const data = await response.json();
-          throw new Error(data.error || "Impossible de récupérer les données de l'utilisateur");
+          throw new Error("Impossible de récupérer les informations de l'utilisateur");
         }
 
-        const data = await response.json();
-        this.user = data;
+        const { user } = await response.json();
+        this.user = user;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+    async logout() {
+      try {
+        const response = await fetch('http://localhost:3003/user/logout', {
+          method: 'POST',
+          credentials: 'include' 
+        });
+
+        if (!response.ok) {
+          throw new Error("Erreur lors de la déconnexion");
+        }
+
+        this.user = null;
+
       } catch (error) {
         console.error(error.message);
       }
