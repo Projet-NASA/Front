5901b53d72dee27a993cb9d90e5b21a5b850adbd
Stanewolfer <seiya0500@gmail.com>
2024-04-11 11:31:29 +0200

    hover sur lien pour accéder à un profil utliisateur

--- components/Feed/FeedGet.vue
+++ components/Feed/FeedGet.vue
@@ -1,60 +1,48 @@
 <template>
   <div>
-    <div
-      v-for="post in reversedPosts"
-      :key="post.id"
-      class="p-4 bg-secondary-200 rounded shadow mb-4"
-    >
-      <NuxtLink
-        :to="`/profile/${post.user.id}`"
-        class="flex items-center mb-2">
+    <div v-for="post in reversedPosts" :key="post.id" class="p-4 bg-secondary-200 rounded shadow mb-4">
+      <div class="flex">
+        <NuxtLink :to="`/profile/${post.user.id}`" class="flex items-center mb-2">
 
-        <img
-          class="w-10 h-10 rounded-full"
-          src="../../public/logo-rounded.png"
-          alt="User avatar"
-        />
+          <img
+            class="w-10 h-10 rounded-full hover:outline hover:outline-primary-default hover:outline-offset-2 click:outline click:outline-primary-default click:outline-offset-2"
+            src="../../public/logo-rounded.png" alt="User avatar" />
+        </NuxtLink>
         <div class="ml-2">
-          <div class="text-text-default font-bold">
-            {{ post.user.firstName }} {{ post.user.lastName }}
-          </div>
+          <NuxtLink :to="`/profile/${post.user.id}`"
+            class="flex items-center hover:text-primary-default hover:underline click:text-primary-default click:underline">
+            <div class=" font-bold">
+              {{ post.user.firstName }} {{ post.user.lastName }}
+            </div>
+          </NuxtLink>
           <div class="text-text-default text-sm text-gray-500">
             {{ timeSince(post.createdAt) }}
           </div>
         </div>
-      </NuxtLink>
+      </div>
       <div class="text-text-default mb-2">{{ post.message }}</div>
       <div class="flex justify-between items-center text-gray-500">
         <button @click="likePost(post)" class="text-lg">
           {{ post.like }}
           <span v-if="post.userliked.some(user => user.userId === userId)">
-            <Icon
-              name="material-symbols:favorite"
-              class="text-primary-default text-2xl hover:animate-ping click:animate-ping"
-            />
+            <Icon name="material-symbols:favorite"
+              class="text-primary-default text-2xl hover:animate-ping click:animate-ping" />
           </span>
           <span v-else>
-            <Icon
-              name="material-symbols:favorite-outline"
-              class="hover:animate-ping hover:text-primary-default click:animate-ping click:text-primary-default text-2xl"
-            />
+            <Icon name="material-symbols:favorite-outline"
+              class="hover:animate-ping hover:text-primary-default click:animate-ping click:text-primary-default text-2xl" />
           </span>
         </button>
         <button class="text-lg">
           {{ post.comments.length }}
-          <Icon
-            name="material-symbols:chat"
-            class="hover:animate-ping hover:text-primary-default click:animate-ping click:text-primary-default cursor-pointer text-2xl"
-          />
+          <Icon name="material-symbols:chat"
+            class="hover:animate-ping hover:text-primary-default click:animate-ping click:text-primary-default cursor-pointer text-2xl" />
         </button>
       </div>
       <FeedComment :postId="`${post.id}`" />
       <div v-if="postComments(post.id).length > 0" class="comments-section">
-        <div
-          v-for="comment in postComments(post.id)"
-          :key="comment.id"
-          class="p-4 bg-secondary-200 rounded shadow mb-4"
-        >
+        <div v-for="comment in postComments(post.id)" :key="comment.id"
+          class="p-4 bg-secondary-200 rounded shadow mb-4">
           <div class="text-text-default mb-2">{{ comment.message }}</div>
         </div>
       </div>
